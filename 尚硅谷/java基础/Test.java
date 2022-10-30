98bdced8686eecd4bc2eb36611350a81d7a4de3b
dalilda <1257958094@qq.com>
2022-10-30 20:35:49 +0800

    2022年10月30日 20:35  星期日

--- 尚硅谷/java基础/Test.java
+++ 尚硅谷/java基础/Test.java
@@ -4,6 +4,9 @@ import java.util.HashSet;
 
 class Test{
     public static void main(String[] args) {
-        
+        System.out.println(100%3);
+        System.out.println(100%3.0);
     }
 }
+
+
