2dcb721b284432369acfa00e134ef9753a600cf4
dalilda <1257958094@qq.com>
2022-11-04 21:28:24 +0800

    2022年11月4日 21:28  星期五

--- 尚硅谷/java基础/Test.java
+++ 尚硅谷/java基础/Test.java
@@ -6,8 +6,11 @@ class Test{
     public static void main(String[] args) {
         String a = new String("20");
         String b = new String("20");
-        System.out.println(a+Character.MIN_VALUE+"|");
-        System.out.println(System.currentTimeMillis());
+        System.out.println(a==b);
+        System.out.println(a.equals(b));
+        // System.out.println(a+Character.MIN_VALUE+"|");
+        // System.out.println(System.currentTimeMillis());
+        Integer.parseInt(b)
     }
 }
 
