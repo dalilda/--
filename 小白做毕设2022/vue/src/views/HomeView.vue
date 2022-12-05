9ca949d5f2003b0b56db9816c5fc4c1499153a87
dalilda <1257958094@qq.com>
2022-12-06 01:28:31 +0800

    侧边栏 点击选中变黄色

--- 小白做毕设2022/vue/src/views/HomeView.vue
+++ 小白做毕设2022/vue/src/views/HomeView.vue
@@ -3,7 +3,9 @@
       <el-aside width="200px" style="background-color: rgb(238, 241, 246); " >
         <el-menu :default-openeds="['1', '3']" style="min-height:100%;overflow-x: hidden;"
         background-color="rgb(48,65,86"
-        text-color="#fff">
+        text-color="#fff"
+        active-text-color="#ffd04b"
+        >
           <el-submenu index="1">
             <template slot="title"><i class="el-icon-message"></i>导航一</template>
             <el-menu-item-group>
