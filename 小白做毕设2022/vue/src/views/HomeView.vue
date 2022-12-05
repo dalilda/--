51dfbe1700676a02fe1da80ad62c1aed67f040be
dalilda <1257958094@qq.com>
2022-12-06 01:25:06 +0800

    侧边栏 1. 背景颜色 2. 文字颜色 3. 宽度参差不齐,用overflow-x:hidden处理

--- 小白做毕设2022/vue/src/views/HomeView.vue
+++ 小白做毕设2022/vue/src/views/HomeView.vue
@@ -1,7 +1,9 @@
 <template>
     <el-container style="height: 100vh; ">
       <el-aside width="200px" style="background-color: rgb(238, 241, 246); " >
-        <el-menu :default-openeds="['1', '3']" style="min-height:100%">
+        <el-menu :default-openeds="['1', '3']" style="min-height:100%;overflow-x: hidden;"
+        background-color="rgb(48,65,86"
+        text-color="#fff">
           <el-submenu index="1">
             <template slot="title"><i class="el-icon-message"></i>导航一</template>
             <el-menu-item-group>
