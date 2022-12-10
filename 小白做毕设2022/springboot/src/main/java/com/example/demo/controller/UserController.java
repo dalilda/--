64559ddd28632be9083661ade05ee47dfcffd613
dalilda <1257958094@qq.com>
2022-12-11 01:42:10 +0800

    1. controller调用service 2. service调用mapper 3. service的save方法,即是insert也是update

--- 小白做毕设2022/springboot/src/main/java/com/example/demo/controller/UserController.java
+++ 小白做毕设2022/springboot/src/main/java/com/example/demo/controller/UserController.java
@@ -2,6 +2,7 @@ package com.example.demo.controller;
 
 import com.example.demo.entity.User;
 import com.example.demo.mapper.UserMapper;
+import com.example.demo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,12 +14,16 @@ public class UserController {
     @Autowired
     private UserMapper userMapper;
 
+    @Autowired
+    private UserService userService;
+
     @PostMapping
     public Integer save(@RequestBody User user){
-        return userMapper.insert(user);
+//        新增或者更新都在里面
+        return userService.save(user);
     }
 
-    @GetMapping("/")
+    @GetMapping()
     public List<User> index(){
         List<User> userList = userMapper.findAll();
         return userList;
--- 小白做毕设2022/springboot/src/main/java/com/example/demo/mapper/UserMapper.java
+++ 小白做毕设2022/springboot/src/main/java/com/example/demo/mapper/UserMapper.java
@@ -4,6 +4,7 @@ import com.example.demo.entity.User;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 
 import java.util.List;
 
@@ -15,4 +16,7 @@ public interface UserMapper  {
     @Insert("insert into sys_user (username,password,nickname,email,phone,address)\n" +
             "Values(#{username},#{password},#{nickname},#{email},#{phone},#{address})")
     int insert(User user);
+
+    @Update("update sys_user set username = #{username},password=#{password},nickname=#{nickname},email=#{email},phone=#{phone},address=#{address} where id=#{id}")
+    int update(User user);
 }
