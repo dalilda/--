a21750de482bdf1af0fb629311759f2ed664860a
dalilda <1257958094@qq.com>
2022-12-11 01:24:47 +0800

    1. post请求save方法 2. 使用postman请求

--- 小白做毕设2022/springboot/src/main/java/com/example/demo/controller/UserController.java
+++ 小白做毕设2022/springboot/src/main/java/com/example/demo/controller/UserController.java
@@ -3,9 +3,7 @@ package com.example.demo.controller;
 import com.example.demo.entity.User;
 import com.example.demo.mapper.UserMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -14,6 +12,12 @@ import java.util.List;
 public class UserController {
     @Autowired
     private UserMapper userMapper;
+
+    @PostMapping
+    public Integer save(@RequestBody User user){
+        return userMapper.insert(user);
+    }
+
     @GetMapping("/")
     public List<User> index(){
         List<User> userList = userMapper.findAll();
--- 小白做毕设2022/springboot/src/main/java/com/example/demo/mapper/UserMapper.java
+++ 小白做毕设2022/springboot/src/main/java/com/example/demo/mapper/UserMapper.java
@@ -1,6 +1,7 @@
 package com.example.demo.mapper;
 
 import com.example.demo.entity.User;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
@@ -10,4 +11,8 @@ import java.util.List;
 public interface UserMapper  {
     @Select("SELECT * FROM sys_user;")
     List<User> findAll();
+
+    @Insert("insert into sys_user (username,password,nickname,email,phone,address)\n" +
+            "Values(#{username},#{password},#{nickname},#{email},#{phone},#{address})")
+    int insert(User user);
 }
