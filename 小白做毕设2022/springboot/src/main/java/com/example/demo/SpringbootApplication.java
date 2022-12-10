bd7bd626c0c7066335997eae404606e77299b431
dalilda <1257958094@qq.com>
2022-12-11 00:49:08 +0800

    创建mapper,创建controller,测试mybatis

--- 小白做毕设2022/springboot/src/main/java/com/example/demo/SpringbootApplication.java
+++ 小白做毕设2022/springboot/src/main/java/com/example/demo/SpringbootApplication.java
@@ -1,23 +1,23 @@
 package com.example.demo;
 
 import com.example.demo.entity.User;
+import com.example.demo.mapper.UserMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+import java.util.List;
+
+
 @SpringBootApplication
 public class SpringbootApplication {
+
 
     public static void main(String[] args) {
         SpringApplication.run(SpringbootApplication.class, args);
     }
 
-    @GetMapping("/")
-    public String index(){
-        User user = new User();
-        user.setId(1);
-        return "ok";
-    }
+
 }
---
+++ 小白做毕设2022/springboot/src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,21 @@
+package com.example.demo.controller;
+
+import com.example.demo.entity.User;
+import com.example.demo.mapper.UserMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class UserController {
+    @Autowired
+    private UserMapper userMapper;
+    @GetMapping("/")
+    public List<User> index(){
+        List<User> userList = userMapper.findAll();
+        return userList;
+//        return userMapper.findAll();
+    }
+}
